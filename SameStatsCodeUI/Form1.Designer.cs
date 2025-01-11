b36df4a45e72acad3dd1e0bc160ce765b8de8fb2
JaneConan <JaneConan@live.com>
2025-01-11 18:25:44 +0800

    实现绘制你的数据

--- SameStatsCodeUI/Form1.Designer.cs
+++ SameStatsCodeUI/Form1.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            tabPage2 = new TabPage();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)webView21).BeginInit();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(12, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(776, 426);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(webView21);
+            tabPage1.Location = new Point(4, 33);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(768, 389);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "绘制你的数据";
+            tabPage1.UseVisualStyleBackColor = true;
+            tabPage1.HandleCreated += new EventHandler(tabPage1_HandleCreated);
+            // 
+            // webView21
+            // 
+            webView21.AllowExternalDrop = true;
+            webView21.CreationProperties = null;
+            webView21.DefaultBackgroundColor = Color.White;
+            webView21.Location = new Point(0, 0);
+            webView21.Name = "webView21";
+            webView21.Size = new Size(765, 389);
+            webView21.TabIndex = 0;
+            webView21.ZoomFactor = 1D;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 33);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(768, 389);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "运行你的程序";
+            tabPage2.UseVisualStyleBackColor = true;
+            tabPage2.HandleCreated += new EventHandler(tabPage2_HandleCreated);
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 24F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Form1";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)webView21).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
     }
 }
--- SameStatsCodeUI/Form1.cs
+++ SameStatsCodeUI/Form1.cs
@@ -1,3 +1,5 @@
+using Microsoft.Web.WebView2.Core;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace SameStatsCodeUI
@@ -15,17 +17,34 @@ namespace SameStatsCodeUI
         public Form1()
         {
             InitializeComponent();
+            this.ClientSize = new Size(1280, 720);
+            tabControl1.Dock = DockStyle.Fill;
+            tabPage1.Dock = DockStyle.Fill;
+            tabPage2.Dock = DockStyle.Fill;
+        }
+
+        private async void tabPage1_HandleCreated(object sender, EventArgs e)
+        {
+            // 设置窗体大小
+            webView21.Dock = DockStyle.Fill;
+            await webView21.EnsureCoreWebView2Async(null);
+            webView21.CoreWebView2.Navigate(Path.Combine(AppContext.BaseDirectory, "drawmydata", "index.html"));
+            webView21.WebMessageReceived += webView2_WebMessageReceived;
+        }
+
+        private async void tabPage2_HandleCreated(object sender, EventArgs e)
+        {
             // 获取程序的当前工作目录并构建PythonCode文件夹的路径
             string programPath = Application.StartupPath;
             pythonCodeFolderPath = Path.Combine(programPath, "PythonCode");
 
             // 设置窗体大小
-            this.ClientSize = new Size(800, 600);
+            //tabPage2.ClientSize = new Size(800, 600);
             // 表单设置
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "SameStatsCodeUI";
-            this.Text = "Python代码运行与图片查看器";
+            this.Text = "SameStatsCodeUI";
             this.ResumeLayout(false);
 
             // 添加运行按钮
@@ -36,35 +55,67 @@ namespace SameStatsCodeUI
                 Size = new Size(150, 30)
             };
             runButton.Click += RunButton_Click;
-            this.Controls.Add(runButton);
+            tabPage2.Controls.Add(runButton);
 
             // 添加显示运行状态的Label
             this.statusLabel = new Label
             {
-                Location = new Point(10, 130),
+                Location = new Point(10, 60),
                 AutoSize = true
             };
-            this.Controls.Add(this.statusLabel);
-
-            // 添加图片显示标签
-            imageLabel = new Label
-            {
-                Location = new Point(10, 170),
-                Size = new Size(760, 400)
-            };
-            imageLabel.AutoSize = false;
-            imageLabel.TextAlign = ContentAlignment.MiddleLeft;
-            this.Controls.Add(imageLabel);
+            tabPage2.Controls.Add(this.statusLabel);
 
             // 添加显示当前图片名称和序号的Label
             this.currentImageNameLabel = new Label
             {
-                Location = new Point(10, 570),
+                Location = new Point(10, 100),
                 AutoSize = true
             };
-            this.Controls.Add(this.currentImageNameLabel);
+            tabPage2.Controls.Add(this.currentImageNameLabel);
+
+            // 添加图片显示标签
+            imageLabel = new Label
+            {
+                Location = new Point(10, 20),
+                Size = new Size(760, 400)
+            };
+            imageLabel.AutoSize = false;
+            imageLabel.TextAlign = ContentAlignment.MiddleLeft;
+            tabPage2.Controls.Add(imageLabel);
+
         }
 
+        private async void webView2_WebMessageReceived(object sender, CoreWebView2WebMessageReceivedEventArgs e)
+        {
+            var data = e.WebMessageAsJson;
+            var jsonData = JsonConvert.DeserializeObject<dynamic>(data);
+            string filename = jsonData.filename;
+            string content = jsonData.content;
+
+            // 这里处理文件保存逻辑
+            SaveCsvFileLocal(filename, content);
+        }
+
+        private void SaveCsvFileLocal(string filename, string content)
+        {
+            try
+            {
+                // 获取程序运行目录
+                var basePath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                var savePath = Path.Combine(basePath, "PythonCode", "seed_datasets", filename);
+                Directory.CreateDirectory(Path.GetDirectoryName(savePath));
+                // 去除内容中的前缀部分并写入文件
+                var actualContent = content.Replace("data:text/csv;charset=utf - 8,", "");
+                File.WriteAllText(savePath, actualContent);
+                MessageBox.Show("文件保存成功");
+                // 自动打开文件夹
+                System.Diagnostics.Process.Start("explorer.exe", Path.GetDirectoryName(savePath));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"保存文件时出错: {ex.Message}");
+            }
+        }
 
         private async void RunButton_Click(object sender, EventArgs e)
         {
@@ -109,7 +160,8 @@ namespace SameStatsCodeUI
             startInfo.WorkingDirectory = pythonCodeFolderPath;
 
             // 使用Invoke确保在UI线程中更新Label
-            this.Invoke((MethodInvoker)(() => {
+            this.Invoke((MethodInvoker)(() =>
+            {
                 statusLabel.Text = "开始执行Python脚本";
             }));
 
@@ -124,7 +176,8 @@ namespace SameStatsCodeUI
                         if (!string.IsNullOrEmpty(e.Data))
                         {
                             Console.WriteLine("输出: " + e.Data);
-                            this.Invoke((MethodInvoker)(() => {
+                            this.Invoke((MethodInvoker)(() =>
+                            {
 
                                 statusLabel.Text = "输出: " + e.Data;
                             }));
@@ -136,7 +189,8 @@ namespace SameStatsCodeUI
                         if (!string.IsNullOrEmpty(e.Data))
                         {
                             Console.WriteLine("错误: " + e.Data);
-                            this.Invoke((MethodInvoker)(() => {
+                            this.Invoke((MethodInvoker)(() =>
+                            {
                                 statusLabel.Text = "错误: " + e.Data;
                             }));
                         }
